b6a50a97e104232408c3db06b24640f9a3e9b388
Tim Cheung <tim.cheung@arctiq.ca>
2023-09-07 21:00:51 -0400

    initial commit

---
+++ eks.tf
@@ -0,0 +1,64 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com", "ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "eksdemo" {
+  name               = "eksdemorole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "eksdemofullaccess" {
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  role       = aws_iam_role.eksdemo.name
+}
+
+resource "aws_iam_role_policy_attachment" "eksdemoclusterpolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eksdemo.name
+}
+
+resource "aws_iam_role_policy_attachment" "eksdemonodepolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eksdemo.name
+}
+
+resource "aws_iam_role_policy_attachment" "eksdemoecrpolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eksdemo.name
+}
+
+resource "aws_eks_cluster" "demo" {
+  name     = "demo"
+  role_arn = aws_iam_role.eksdemo.arn
+
+  vpc_config {
+    subnet_ids = module.vpc.private_subnets
+  }
+}
+
+resource "aws_eks_node_group" "example" {
+  cluster_name    = aws_eks_cluster.demo.name
+  node_group_name = "demo"
+  node_role_arn   = aws_iam_role.eksdemo.arn
+  subnet_ids      = module.vpc.private_subnets
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+}
